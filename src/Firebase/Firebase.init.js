954f7f5055e7be35d3afafc5bb11dbbb379a06b7
Ashikur_Rahman <ashikur35-540@diu.edu.bd>
2022-10-27 21:54:00 +0600

    env variable added

--- src/Firebase/Firebase.init.js
+++ src/Firebase/Firebase.init.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDyRGT7rstX9ZcUejZw7mRuatILdAxW_tc",
-    authDomain: "learners-hut.firebaseapp.com",
-    projectId: "learners-hut",
-    storageBucket: "learners-hut.appspot.com",
-    messagingSenderId: "661386820500",
-    appId: "1:661386820500:web:a7434bce9cedaafd0e6d81"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
